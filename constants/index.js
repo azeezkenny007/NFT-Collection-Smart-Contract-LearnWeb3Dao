d00e31ab543c4951602a09f8b8e598734a881dd5
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-20 22:59:39 +0100

    added the address, and abi that is used to verify and deploy the contract

---
+++ constants/index.js
@@ -0,0 +1,19 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x50a682e60988AaABF01978E759470A5968933094";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+const mumbaiAddress = "0x50a682e60988AaABF01978E759470A5968933094";
+const goerliAddress = "0xB6baa8FABDac9e01196Faafcc9C2d36964eAcF71";
+
+const crytoDevGoerliAddress= "0x9d6510Ba85F8587e39b71FFE197c2093e9cBcB05"
+const crytoDevPolygonAddress ="0x61DC32BF84A97fC259C6B5997D2efA602CCdc745"
+
+module.exports = {
+  WHITELIST_CONTRACT_ADDRESS,
+  METADATA_URL,
+  mumbaiAddress,
+  goerliAddress,
+  crytoDevGoerliAddress,
+  crytoDevPolygonAddress
+};
