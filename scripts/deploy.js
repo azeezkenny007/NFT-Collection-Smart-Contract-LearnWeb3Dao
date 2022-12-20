67acb5e11fdf92c673e7a6c5acef3c3cae0b88f7
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-20 23:00:39 +0100

    the script nneded to deploy the contract

---
+++ scripts/deploy.js
@@ -0,0 +1,33 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+
+const { METADATA_URL,WHITELIST_CONTRACT_ADDRESS } = require("../constants");
+const { ethers } = require("hardhat");
+
+async function main() {
+   const CryptoDevFactory = await ethers.getContractFactory("CryptoDevs")
+   const CryptoDev = await CryptoDevFactory.deploy(METADATA_URL,WHITELIST_CONTRACT_ADDRESS)
+   await CryptoDev.deployed()
+   console.log('---------- contract has been deployed hurray🚀🚀🚀🚀 ------------------')
+   console.log('--------------------------------------------------------')
+   console.log(`The CrytoDev contract has been deployed to ${CryptoDev.address}`)
+   return CryptoDev
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().then((e)=>{
+  console.log(e)
+}).catch((e) => {
+  console.log(e);
+  process.exitCode = 1;
+});
+
+
+module.exports={
+   main
+}
