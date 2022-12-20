0f387684159de48bc74eb24f1e5b4266a19ec802
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-20 22:57:13 +0100

    added the arguments needed to verify the contract

---
+++ arguments.js
@@ -0,0 +1,7 @@
+ const { mumbaiAddress ,METADATA_URL,crytoDevGoerliAddress,crytoDevPolygonAddress,WHITELIST_CONTRACT_ADDRESS} = require("./constants");
+ 
+module.exports=[
+  METADATA_URL,
+  WHITELIST_CONTRACT_ADDRESS
+]
+
